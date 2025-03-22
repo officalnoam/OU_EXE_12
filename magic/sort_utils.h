a4153ee44610db300b68f50589778cedd8e2d315
Noam <noamrooffical@gmail.com>
2025-03-22 22:41:55 +0200

    Fix get_arr_length function decleration

--- magic/sort_utils.h
+++ magic/sort_utils.h
@@ -2,7 +2,7 @@
   #define SORT_UTILS
   
   void swap(int* arr, int i, int j);
-  int get_array_size(int* arr);
+  int get_arr_length(int* arr);
   int partition(int* arr, int low, int high);
   void quick_sort(int* arr, int low, int high);
   int* get_sorted_arr_copy(int* arr);
